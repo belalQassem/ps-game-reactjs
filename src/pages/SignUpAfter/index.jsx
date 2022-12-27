63bad4383ef5a1aeb92d944fc12aa63fa0036fe8
belalQassem <belalqassem52@gmail.com>
2022-12-27 10:26:11 +0200

    pages

---
+++ src/pages/SignUpAfter/index.jsx
@@ -0,0 +1,15 @@
+import SideLeft from "../../components/SideLeft";
+import './style.css'
+import React from 'react'
+import SideRight from "../../components/SideLeft/SideRight";
+
+export default function SignUpAfter() {
+  return (
+    <div className="SignUpAfter">
+    <SideLeft/>
+    <SideRight/>
+    </div>
+    
+
+  )
+}
---
+++ src/pages/SignUpBefore/index.jsx
@@ -0,0 +1,20 @@
+import './style.css'
+import React, { Component } from 'react'
+import SideLeft from '../../components/SideLeft'
+import Lay from '../../components/Lay'
+
+
+
+
+export default class SignUpBefore extends Component {
+  render() {
+    return (
+      <div className='SignUpBefore'>
+            <Lay>
+            <SideLeft/>
+            </Lay>
+            
+      </div>
+    )
+  }
+}
