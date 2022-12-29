985566ba4b8419b4eb8aed9f75dbae485bcdcfaa
belalQassem <belalqassem52@gmail.com>
2022-12-29 10:25:21 +0200

    update sign up before

--- src/pages/SignUpBefore/index.jsx
+++ src/pages/SignUpBefore/index.jsx
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 import SideLeft from '../../components/SideLeft'
 import Lay from '../../components/Lay'
 
+import SideLeftBlue from '../../components/SideLeftBlue'
+import FromRightBlue from '../../components/FromRightBlue'
+
 
 
 
@@ -10,9 +13,14 @@ export default class SignUpBefore extends Component {
   render() {
     return (
       <div className='SignUpBefore'>
-            <Lay>
-            <SideLeft/>
-            </Lay>
+        <SideLeftBlue/>
+        <FromRightBlue/>
+
+            {/* <LoginForm/> */}
+            {/* <Lay> */}
+              
+            {/* <SideLeft/> */}
+            {/* </Lay> */}
             
       </div>
     )
